08205ceca300b2fefbe3f242fe9b84a9ed97d45a
demo <nasima365banu@gmail.com>
2026-06-27 22:18:08 +0530

    Added Command Pattern implementation

---
+++ src/com/cognizant/Command/Light.java
@@ -0,0 +1,11 @@
+package com.cognizant.Command;
+
+public class Light {
+    public void turnOn() {
+        System.out.println("Light ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Light OFF");
+    }
+}
